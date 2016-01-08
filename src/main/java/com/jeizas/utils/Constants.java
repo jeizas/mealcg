4eb27f599e6f28d1e5c5ee937a35ff9f5ef16d4d
jeizas <jeizas1210@gmail.com>
2016-01-09 02:13:51 +0800

    U constants

--- src/main/java/com/jeizas/utils/Constants.java
+++ src/main/java/com/jeizas/utils/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
 	public static final Integer DELETED_NO = 0;
 	public static final Integer DELETED_YES = 1;
 	
-	public static final String FOOD_URL = "/resource/mealface/";
+	public static final String FOOD_URL = "/resource/mealface1/";
 
 }
